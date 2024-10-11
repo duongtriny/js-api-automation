077046b056e4de5a7540acfcff9bb02a6342871e
Duong Trinh <duong.triny@gmail.com>
2024-10-11 22:20:40 +0700

    [ID-466] GraphQL

---
+++ tests/film-query/film-query.spec.js
@@ -0,0 +1,21 @@
+const { readFile } = require('node:fs/promises');
+const { resolve } = require('node:path');
+const { test, expect } = require('@playwright/test');
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+test("Verify user query data successful", async ({ request }) => {
+    const filePath = resolve('data/film-query/get-all-fields.graphql');
+    const query = await readFile(filePath, { encoding: 'utf8' });
+    const requestBody = { query };
+    const response = await request.post("https://swapi-graphql.netlify.app/.netlify/functions/index", {
+        data: requestBody
+    });
+    const responseJson = await response.json();
+    console.log("Response data: ", responseJson);
+    expect(response.status()).toBe(200);
+    //schema
+    //check expectation
+    //neu co dynamic data thi can tach ra de verify
+
+});
